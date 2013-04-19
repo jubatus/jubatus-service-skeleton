1608266e48d8b7a971c106fa80959f19a509f9a6
y_oda <oda@onicos.co.jp>
2013-04-19 17:42:23 +0900

    regenerate *.tmpl.* files by new jubatus 0.4.3 jenerator

--- kvs_serv.tmpl.cpp
+++ kvs_serv.tmpl.cpp
@@ -1,19 +1,30 @@
 // This file is auto-generated from kvs.idl
-// *** DO NOT EDIT ***
 
 #include "kvs_serv.hpp"
 
 namespace jubatus {
 namespace server {
 
-kvs_serv::kvs_serv(const jubatus::framework::server_argv& a)
-    : jubatus::framework::jubatus_serv(a) {
+kvs_serv::kvs_serv(
+  const jubatus::framework::server_argv& a,
+  const common::cshared_ptr<common::lock_service>& zk)
+    : jubatus::framework::server_base(a) {
   // somemixable* mi = new somemixable;
   // somemixable_.set_model(mi);
-  // register_mixable(mi);
+  // get_mixable_holder()->register_mixable(mi);
 }
 
 kvs_serv::~kvs_serv() {
+}
+
+virtual mixer::mixer* kvs_serv::get_mixer() const {
+}
+
+pfi::lang::shared_ptr<framework::mixable_holder> kvs_serv::get_mixable_holder(
+    ) const {
+}
+
+void kvs_serv::get_status(status_t& status) const {
 }
 
 bool kvs_serv::put(const std::string& key, const std::string& value) {
@@ -26,16 +37,6 @@ bool kvs_serv::del(const std::string& key) {
 }
 
 bool kvs_serv::clear() {
-}
-
-std::map<std::string, std::map<std::string, std::string> > kvs_serv::get_status(
-    ) const {
-}
-
-bool kvs_serv::save(const std::string& id) {
-}
-
-bool kvs_serv::load(const std::string& id) {
 }
 
 }  // namespace server
--- kvs_serv.tmpl.hpp
+++ kvs_serv.tmpl.hpp
@@ -1,5 +1,4 @@
 // This file is auto-generated from kvs.idl
-// *** DO NOT EDIT ***
 
 #ifndef KVS_SERV_TMPL_HPP_
 #define KVS_SERV_TMPL_HPP_
@@ -10,17 +9,21 @@
 namespace jubatus {
 namespace server {
 
-class kvs_serv : public jubatus::framework::jubatus_serv {  // do not change
+class kvs_serv : public jubatus::framework::server_base {  // do not change
  public:
-  kvs_serv(const jubatus::framework::server_argv& a);  // do not change
+  kvs_serv(
+    const jubatus::framework::server_argv& a,
+    const common::cshared_ptr<common::lock_service>& zk);  // do not change
   virtual ~kvs_serv();  // do not change
+
+  virtual mixer::mixer* get_mixer() const;
+  pfi::lang::shared_ptr<framework::mixable_holder> get_mixable_holder() const;
+  void get_status(status_t& status) const;
+
   bool put(const std::string& key, const std::string& value);
   std::string get(const std::string& key) const;
   bool del(const std::string& key);
   bool clear();
-  std::map<std::string, std::map<std::string, std::string> > get_status() const;
-  bool save(const std::string& id);
-  bool load(const std::string& id);
 
  private:
   // add user data here like: pfi::lang::shared_ptr<some_type> some_;
