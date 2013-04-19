8f6a18945bd1414425f674fcfee4970cbab3a0b0
y_oda <oda@onicos.co.jp>
2013-04-19 18:37:58 +0900

    rewrite kvs_serv.* based on kvs_serv.tmpl.*
    + fix kvs_serv.tmpl.* compilation errors

--- kvs_serv.cpp
+++ kvs_serv.cpp
@@ -1,17 +1,16 @@
-// this is automatically generated template header please implement and edit.
+// This file is auto-generated from kvs.idl
 
 #include "kvs_serv.hpp"
 #include <glog/logging.h>
 
-using namespace jubatus::framework;
-
-namespace jubatus { namespace server { // do not change
+namespace jubatus {
+namespace server {
 
 namespace {
-/*
-  These parameters are unused; written to show you how to handle config.
-  Also see `set_config` method below.
-*/
+
+// These parameters are unused; written to show you how to handle config.
+// Also see `set_config` method below.
+
 struct kvs_serv_config {
   int param1;
   std::string param2;
@@ -25,66 +24,67 @@ struct kvs_serv_config {
 };
 }
 
-kvs_serv::kvs_serv(const server_argv& a,
-                   const jubatus::common::cshared_ptr<jubatus::common::lock_service>& zk)
-  :server_base(a)
-{
-  mixer_.reset(mixer::create_mixer(a, zk));
+kvs_serv::kvs_serv(
+  const jubatus::framework::server_argv& a,
+  const common::cshared_ptr<common::lock_service>& zk)
+    : jubatus::framework::server_base(a) {
+  mixer_.reset(framework::mixer::create_mixer(a, zk));
 
-  mixable_holder_.reset(new mixable_holder());
+  mixable_holder_.reset(new framework::mixable_holder());
   mixer_->set_mixable_holder(mixable_holder_);
 
   data_ = kvs_map_t();
 }
 
-kvs_serv::~kvs_serv()
-{}
-
-//update, cht(2)
-bool kvs_serv::put(const std::string& key, const std::string& value)
-{
-    if (data_.count(key) == 0) {
-        data_.insert(std::make_pair(key, value));
-        return true;
-    }
-    return false;
+kvs_serv::~kvs_serv() {
 }
 
-//analysis, cht(2)
-std::string kvs_serv::get(const std::string& key) const
-{
-    kvs_map_t::const_iterator it = data_.find(key);
-    if (it == data_.end()) {
-        throw std::runtime_error("key not found");
-    }
-    return it->second;
+framework::mixer::mixer* kvs_serv::get_mixer() const {
+  return mixer_.get();
 }
 
-//update, cht(2)
-bool kvs_serv::del(const std::string& key)
-{
-    data_.erase(key);
+pfi::lang::shared_ptr<framework::mixable_holder> kvs_serv::get_mixable_holder(
+    ) const {
+  return mixable_holder_;
+}
+
+void kvs_serv::get_status(status_t& status) const {
+  std::stringstream ss;
+  ss << data_.size();
+  status.insert(std::make_pair("size", ss.str()));
+}
+
+bool kvs_serv::put(const std::string& key, const std::string& value) {
+  if (data_.count(key) == 0) {
+    data_.insert(std::make_pair(key, value));
     return true;
+  }
+  return false;
 }
 
-//update, broadcast
-bool kvs_serv::clear()
-{
-    data_.clear();
-    return true;
+std::string kvs_serv::get(const std::string& key) const {
+  kvs_map_t::const_iterator it = data_.find(key);
+  if (it == data_.end()) {
+    throw std::runtime_error("key not found");
+  }
+  return it->second;
 }
 
-//analysis, broadcast
-void kvs_serv::get_status(status_t& status) const
-{
-    std::stringstream ss;
-    ss << data_.size();
-    status.insert(std::make_pair("size", ss.str()));
+bool kvs_serv::del(const std::string& key) {
+  data_.erase(key);
+  return true;
+}
+
+bool kvs_serv::clear() {
+  data_.clear();
+  return true;
 }
 
 bool kvs_serv::set_config(const std::string& config) {
-  jsonconfig::config config_root(pfi::lang::lexical_cast<pfi::text::json::json>(config));
-  kvs_serv_config conf = jsonconfig::config_cast_check<kvs_serv_config>(config_root);
+  jsonconfig::config config_root(
+      pfi::lang::lexical_cast<pfi::text::json::json>(config));
+  kvs_serv_config conf =
+      jsonconfig::config_cast_check<kvs_serv_config>(config_root);
 
   // You can use configuration values here
   LOG(INFO) << "param1 = " << conf.param1;
@@ -93,4 +93,5 @@ bool kvs_serv::set_config(const std::string& config) {
   return true;
 }
 
-}} // namespace jubatus::server
+}  // namespace server
+}  // namespace jubatus
--- kvs_serv.hpp
+++ kvs_serv.hpp
@@ -1,39 +1,34 @@
-// this is automatically generated template header please implement and edit.
-#pragma once
+// This file is auto-generated from kvs.idl
+
+#ifndef KVS_SERV_HPP_
+#define KVS_SERV_HPP_
+
 #include <jubatus/framework.hpp>
 #include <jubatus/framework/mixer/mixer_factory.hpp>
 #include "kvs_types.hpp"
-using namespace jubatus::framework;
 
-namespace jubatus { namespace server { // do not change
-class kvs_serv : public server_base // do not change
-{
-public:
-  kvs_serv(const server_argv& a,
-           const jubatus::common::cshared_ptr<jubatus::common::lock_service>& zk); // do not change
-  virtual ~kvs_serv(); // do not change
+namespace jubatus {
+namespace server {
 
-  mixer::mixer* get_mixer() const {
-    return mixer_.get();
-  }
+class kvs_serv : public jubatus::framework::server_base {  // do not change
+ public:
+  kvs_serv(
+    const jubatus::framework::server_argv& a,
+    const common::cshared_ptr<common::lock_service>& zk);  // do not change
+  virtual ~kvs_serv();  // do not change
 
-  pfi::lang::shared_ptr<framework::mixable_holder> get_mixable_holder() const {
-    return mixable_holder_;
-  }
+  virtual framework::mixer::mixer* get_mixer() const;
+  pfi::lang::shared_ptr<framework::mixable_holder> get_mixable_holder() const;
+  void get_status(status_t& status) const;
 
-  bool put(const std::string& key, const std::string& value); //update cht(2)
-
-  std::string get(const std::string& key) const; //analysis cht(2)
-
-  bool del(const std::string& key); //update cht(2)
-
-  bool clear(); //update broadcast
-
-  void get_status(status_t& status) const; //analysis broadcast
+  bool put(const std::string& key, const std::string& value);
+  std::string get(const std::string& key) const;
+  bool del(const std::string& key);
+  bool clear();
 
   bool set_config(const std::string& config);
 
-private:
+ private:
   pfi::lang::scoped_ptr<framework::mixer::mixer> mixer_;
   pfi::lang::shared_ptr<framework::mixable_holder> mixable_holder_;
 
@@ -41,4 +36,8 @@ private:
   typedef std::map<std::string,std::string> kvs_map_t;
   kvs_map_t data_;
 };
-}} // namespace jubatus::server
+
+}  // namespace server
+}  // namespace jubatus
+
+#endif  // KVS_SERV_HPP_
